b29450919ead6508a164ad5de13d86b0178dfd45
Michael Hacker <hack1973@swbell.net>
2017-03-30 12:02:59 -0500

    Initial commit;Basic form, layout and submit button

---
+++ main.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+
+page_header = """</DOCTYPE html>
+            <html>
+                <head>
+                    <title>User Signup
+                    </title>
+                    <style type="text/css">
+                        .error {
+                            color: red;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <h1>Signup
+                    </h1>
+"""
+
+page_footer = """</body>
+            </html>
+"""
+
+main_content = """  <form method="post">
+                        <table>
+                            <tr>
+                                <td>
+                                    <label for="username">Username</label>                                        </td>
+                                <td>
+                                    <input name="username" type="text" value required>
+                                    <span class="error"></span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label for="password">Password</label>
+                                </td>
+                                <td>
+                                    <input name="password" type="password" required>
+                                    <span class="error"></span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label for="verify">Verify Password</label>
+                                </td>
+                                <td>
+                                    <input name="verify" type="password" required>
+                                    <span class="error"></span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label for="email">Email (Optional)</label>
+                                </td>
+                                <td>
+                                    <input name="email" type="email" value>
+                                    <span class="error"></span>
+                                </td>
+                            </tr>
+                        </table>
+                        <input type= "submit">
+                    </form>
+"""
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+
+        content = page_header + main_content + page_footer
+        self.response.write(content)
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
